748966658aaae195fb8517f46ac65774c422ed1d
tiferet <tiferet@algotec.co.il>
2017-12-21 10:47:50 +0200

    Add an option whether to use a weight decay L2 normalization in all convolutions in the network.

--- keras/applications/inception_resnet_v2.py
+++ keras/applications/inception_resnet_v2.py
@@ -32,6 +32,7 @@ from ..layers import Input
 from ..layers import Lambda
 from ..layers import MaxPooling2D
 from ..layers import Dropout
+from ..regularizers import l2
 from ..utils.data_utils import get_file
 from ..engine.topology import get_source_inputs
 from . import imagenet_utils
@@ -61,6 +62,7 @@ def conv2d_bn(x,
               strides=1,
               padding='same',
               activation='relu',
+              regularization=0.,
               use_bias=False,
               name=None):
     """Utility function to apply conv + BN.
@@ -72,6 +74,8 @@ def conv2d_bn(x,
         strides: strides in `Conv2D`.
         padding: padding mode in `Conv2D`.
         activation: activation in `Conv2D`.
+        regularization: The L2 weight decay coefficient used in the convolutional
+            layers to prevent overfitting. The reference paper uses no regularization.
         use_bias: whether to use a bias in `Conv2D`.
         name: name of the ops; will become `name + '_ac'` for the activation
             and `name + '_bn'` for the batch norm layer.
@@ -84,6 +88,7 @@ def conv2d_bn(x,
                strides=strides,
                padding=padding,
                use_bias=use_bias,
+               kernel_regularizer=l2(regularization),
                name=name)(x)
     if not use_bias:
         bn_axis = 1 if K.image_data_format() == 'channels_first' else 3
@@ -95,7 +100,7 @@ def conv2d_bn(x,
     return x
 
 
-def inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
+def inception_resnet_block(x, scale, block_type, block_idx, activation='relu', regularization=0.):
     """Adds a Inception-ResNet block.
 
     This function builds 3 types of Inception-ResNet blocks mentioned
@@ -122,6 +127,8 @@ def inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
             (see [activations](../activations.md)).
             When `activation=None`, no activation is applied
             (i.e., "linear" activation: `a(x) = x`).
+        regularization: The L2 weight decay coefficient used in the convolutional
+            layers to prevent overfitting. The reference paper uses no regularization.
 
     # Returns
         Output tensor for the block.
@@ -131,24 +138,24 @@ def inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
             `'block17'` or `'block8'`.
     """
     if block_type == 'block35':
-        branch_0 = conv2d_bn(x, 32, 1)
-        branch_1 = conv2d_bn(x, 32, 1)
-        branch_1 = conv2d_bn(branch_1, 32, 3)
-        branch_2 = conv2d_bn(x, 32, 1)
-        branch_2 = conv2d_bn(branch_2, 48, 3)
-        branch_2 = conv2d_bn(branch_2, 64, 3)
+        branch_0 = conv2d_bn(x, 32, 1, regularization=regularization)
+        branch_1 = conv2d_bn(x, 32, 1, regularization=regularization)
+        branch_1 = conv2d_bn(branch_1, 32, 3, regularization=regularization)
+        branch_2 = conv2d_bn(x, 32, 1, regularization=regularization)
+        branch_2 = conv2d_bn(branch_2, 48, 3, regularization=regularization)
+        branch_2 = conv2d_bn(branch_2, 64, 3, regularization=regularization)
         branches = [branch_0, branch_1, branch_2]
     elif block_type == 'block17':
-        branch_0 = conv2d_bn(x, 192, 1)
-        branch_1 = conv2d_bn(x, 128, 1)
-        branch_1 = conv2d_bn(branch_1, 160, [1, 7])
-        branch_1 = conv2d_bn(branch_1, 192, [7, 1])
+        branch_0 = conv2d_bn(x, 192, 1, regularization=regularization)
+        branch_1 = conv2d_bn(x, 128, 1, regularization=regularization)
+        branch_1 = conv2d_bn(branch_1, 160, [1, 7], regularization=regularization)
+        branch_1 = conv2d_bn(branch_1, 192, [7, 1], regularization=regularization)
         branches = [branch_0, branch_1]
     elif block_type == 'block8':
-        branch_0 = conv2d_bn(x, 192, 1)
-        branch_1 = conv2d_bn(x, 192, 1)
-        branch_1 = conv2d_bn(branch_1, 224, [1, 3])
-        branch_1 = conv2d_bn(branch_1, 256, [3, 1])
+        branch_0 = conv2d_bn(x, 192, 1, regularization=regularization)
+        branch_1 = conv2d_bn(x, 192, 1, regularization=regularization)
+        branch_1 = conv2d_bn(branch_1, 224, [1, 3], regularization=regularization)
+        branch_1 = conv2d_bn(branch_1, 256, [3, 1], regularization=regularization)
         branches = [branch_0, branch_1]
     else:
         raise ValueError('Unknown Inception-ResNet block type. '
@@ -162,6 +169,7 @@ def inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
                    K.int_shape(x)[channel_axis],
                    1,
                    activation=None,
+                   regularization=regularization,
                    use_bias=True,
                    name=block_name + '_conv')
 
@@ -181,6 +189,7 @@ def InceptionResNetV2(include_top=True,
                       pooling=None,
                       classes=1000,
                       dropout=0.,
+                      regularization=0.,
                       only_same_padding=False):
     """Instantiates the Inception-ResNet v2 architecture.
 
@@ -228,6 +237,8 @@ def InceptionResNetV2(include_top=True,
         dropout: If greater than zero, adds a dropout layer at the end of the
             network like the reference paper, setting this fraction of input
             units to zero. The reference paper used a dropout rate of 0.2.
+        regularization: The L2 weight decay coefficient used in the convolutional
+            layers to prevent overfitting. The reference paper uses no regularization.
         only_same_padding: If set to True, only 'same' padding is used in all
             network layers, even in places where the reference paper used
             'valid' padding.
@@ -269,23 +280,23 @@ def InceptionResNetV2(include_top=True,
     selected_padding = 'same' if only_same_padding else 'valid'
 
     # Stem block: 35 x 35 x 192
-    x = conv2d_bn(img_input, 32, 3, strides=2, padding=selected_padding)
-    x = conv2d_bn(x, 32, 3, padding=selected_padding)
-    x = conv2d_bn(x, 64, 3)
+    x = conv2d_bn(img_input, 32, 3, strides=2, padding=selected_padding, regularization=regularization)
+    x = conv2d_bn(x, 32, 3, padding=selected_padding, regularization=regularization)
+    x = conv2d_bn(x, 64, 3, regularization=regularization)
     x = MaxPooling2D(3, strides=2, padding=selected_padding)(x)
-    x = conv2d_bn(x, 80, 1, padding=selected_padding)
-    x = conv2d_bn(x, 192, 3, padding=selected_padding)
+    x = conv2d_bn(x, 80, 1, padding=selected_padding, regularization=regularization)
+    x = conv2d_bn(x, 192, 3, padding=selected_padding, regularization=regularization)
     x = MaxPooling2D(3, strides=2, padding=selected_padding)(x)
 
     # Mixed 5b (Inception-A block): 35 x 35 x 320
-    branch_0 = conv2d_bn(x, 96, 1)
-    branch_1 = conv2d_bn(x, 48, 1)
-    branch_1 = conv2d_bn(branch_1, 64, 5)
-    branch_2 = conv2d_bn(x, 64, 1)
-    branch_2 = conv2d_bn(branch_2, 96, 3)
-    branch_2 = conv2d_bn(branch_2, 96, 3)
+    branch_0 = conv2d_bn(x, 96, 1, regularization=regularization)
+    branch_1 = conv2d_bn(x, 48, 1, regularization=regularization)
+    branch_1 = conv2d_bn(branch_1, 64, 5, regularization=regularization)
+    branch_2 = conv2d_bn(x, 64, 1, regularization=regularization)
+    branch_2 = conv2d_bn(branch_2, 96, 3, regularization=regularization)
+    branch_2 = conv2d_bn(branch_2, 96, 3, regularization=regularization)
     branch_pool = AveragePooling2D(3, strides=1, padding='same')(x)
-    branch_pool = conv2d_bn(branch_pool, 64, 1)
+    branch_pool = conv2d_bn(branch_pool, 64, 1, regularization=regularization)
     branches = [branch_0, branch_1, branch_2, branch_pool]
     channel_axis = 1 if K.image_data_format() == 'channels_first' else 3
     x = Concatenate(axis=channel_axis, name='mixed_5b')(branches)
@@ -294,14 +305,15 @@ def InceptionResNetV2(include_top=True,
     for block_idx in range(1, 11):
         x = inception_resnet_block(x,
                                    scale=0.17,
+                                   regularization=regularization,
                                    block_type='block35',
                                    block_idx=block_idx)
 
     # Mixed 6a (Reduction-A block): 17 x 17 x 1088
-    branch_0 = conv2d_bn(x, 384, 3, strides=2, padding=selected_padding)
-    branch_1 = conv2d_bn(x, 256, 1)
-    branch_1 = conv2d_bn(branch_1, 256, 3)
-    branch_1 = conv2d_bn(branch_1, 384, 3, strides=2, padding=selected_padding)
+    branch_0 = conv2d_bn(x, 384, 3, strides=2, padding=selected_padding, regularization=regularization)
+    branch_1 = conv2d_bn(x, 256, 1, regularization=regularization)
+    branch_1 = conv2d_bn(branch_1, 256, 3, regularization=regularization)
+    branch_1 = conv2d_bn(branch_1, 384, 3, strides=2, padding=selected_padding, regularization=regularization)
     branch_pool = MaxPooling2D(3, strides=2, padding=selected_padding)(x)
     branches = [branch_0, branch_1, branch_pool]
     x = Concatenate(axis=channel_axis, name='mixed_6a')(branches)
@@ -310,17 +322,18 @@ def InceptionResNetV2(include_top=True,
     for block_idx in range(1, 21):
         x = inception_resnet_block(x,
                                    scale=0.1,
+                                   regularization=regularization,
                                    block_type='block17',
                                    block_idx=block_idx)
 
     # Mixed 7a (Reduction-B block): 8 x 8 x 2080
-    branch_0 = conv2d_bn(x, 256, 1)
-    branch_0 = conv2d_bn(branch_0, 384, 3, strides=2, padding=selected_padding)
-    branch_1 = conv2d_bn(x, 256, 1)
-    branch_1 = conv2d_bn(branch_1, 288, 3, strides=2, padding=selected_padding)
-    branch_2 = conv2d_bn(x, 256, 1)
-    branch_2 = conv2d_bn(branch_2, 288, 3)
-    branch_2 = conv2d_bn(branch_2, 320, 3, strides=2, padding=selected_padding)
+    branch_0 = conv2d_bn(x, 256, 1, regularization=regularization)
+    branch_0 = conv2d_bn(branch_0, 384, 3, strides=2, padding=selected_padding, regularization=regularization)
+    branch_1 = conv2d_bn(x, 256, 1, regularization=regularization)
+    branch_1 = conv2d_bn(branch_1, 288, 3, strides=2, padding=selected_padding, regularization=regularization)
+    branch_2 = conv2d_bn(x, 256, 1, regularization=regularization)
+    branch_2 = conv2d_bn(branch_2, 288, 3, regularization=regularization)
+    branch_2 = conv2d_bn(branch_2, 320, 3, strides=2, padding=selected_padding, regularization=regularization)
     branch_pool = MaxPooling2D(3, strides=2, padding=selected_padding)(x)
     branches = [branch_0, branch_1, branch_2, branch_pool]
     x = Concatenate(axis=channel_axis, name='mixed_7a')(branches)
@@ -329,16 +342,18 @@ def InceptionResNetV2(include_top=True,
     for block_idx in range(1, 10):
         x = inception_resnet_block(x,
                                    scale=0.2,
+                                   regularization=regularization,
                                    block_type='block8',
                                    block_idx=block_idx)
     x = inception_resnet_block(x,
                                scale=1.,
+                               regularization=regularization,
                                activation=None,
                                block_type='block8',
                                block_idx=10)
 
     # Final convolution block: 8 x 8 x 1536
-    x = conv2d_bn(x, 1536, 1, name='conv_7b')
+    x = conv2d_bn(x, 1536, 1, name='conv_7b', regularization=regularization)
 
     if include_top:
         # Classification block
